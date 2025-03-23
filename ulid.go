98d8a7e42ea44f763d186ab0533da77ac75c1f7e
ICHINOSE Shogo <shogo82148@gmail.com>
2025-03-23 23:33:27 +0900

    optimize ULID text representation for better performance on 32 and 64 bit platforms

--- ulid.go
+++ ulid.go
@@ -221,42 +221,57 @@ func (id ULID) text() [26]byte {
 	var buf [26]byte
 
 	// Optimized unrolled loop ahead.
-	h := uint64(id[0])<<56 | uint64(id[1])<<48 | uint64(id[2])<<40 |
-		uint64(id[3])<<32 | uint64(id[4])<<24 | uint64(id[5])<<16 |
-		uint64(id[6])<<8 | uint64(id[7])
-	l := uint64(id[8])<<56 | uint64(id[9])<<48 | uint64(id[10])<<40 |
-		uint64(id[11])<<32 | uint64(id[12])<<24 | uint64(id[13])<<16 |
-		uint64(id[14])<<8 | uint64(id[15])
+	// Combining 32 bit loads allows the same code to be used
+	// for 32 and 64 bit platforms.
+	a := uint32(id[0])<<16 |
+		uint32(id[1])<<8 |
+		uint32(id[2])
+	b := uint32(id[2])<<24 |
+		uint32(id[3])<<16 |
+		uint32(id[4])<<8 |
+		uint32(id[5])
+	c := uint32(id[6])<<24 |
+		uint32(id[7])<<16 |
+		uint32(id[8])<<8 |
+		uint32(id[9])
+	d := uint32(id[9])<<24 |
+		uint32(id[10])<<16 |
+		uint32(id[11])<<8 |
+		uint32(id[12])
+	e := uint32(id[12])<<24 |
+		uint32(id[13])<<16 |
+		uint32(id[14])<<8 |
+		uint32(id[15])
 
 	// 10 bytes timestamp
-	buf[0] = encoding[(h>>61)&0x1f]
-	buf[1] = encoding[(h>>56)&0x1f]
-	buf[2] = encoding[(h>>51)&0x1f]
-	buf[3] = encoding[(h>>46)&0x1f]
-	buf[4] = encoding[(h>>41)&0x1f]
-	buf[5] = encoding[(h>>36)&0x1f]
-	buf[6] = encoding[(h>>31)&0x1f]
-	buf[7] = encoding[(h>>26)&0x1f]
-	buf[8] = encoding[(h>>21)&0x1f]
-	buf[9] = encoding[(h>>16)&0x1f]
+	buf[0] = encoding[(a>>21)&0x1f]
+	buf[1] = encoding[(a>>16)&0x1f]
+	buf[2] = encoding[(a>>11)&0x1f]
+	buf[3] = encoding[(a>>6)&0x1f]
+	buf[4] = encoding[(a>>1)&0x1f]
+	buf[5] = encoding[(b>>20)&0x1f]
+	buf[6] = encoding[(b>>15)&0x1f]
+	buf[7] = encoding[(b>>10)&0x1f]
+	buf[8] = encoding[(b>>5)&0x1f]
+	buf[9] = encoding[b&0x1f]
 
 	// 16 bytes random
-	buf[10] = encoding[(h>>11)&0x1f]
-	buf[11] = encoding[(h>>6)&0x1f]
-	buf[12] = encoding[(h>>1)&0x1f]
-	buf[13] = encoding[(h<<4|l>>60)&0x1f]
-	buf[14] = encoding[(l>>55)&0x1f]
-	buf[15] = encoding[(l>>50)&0x1f]
-	buf[16] = encoding[(l>>45)&0x1f]
-	buf[17] = encoding[(l>>40)&0x1f]
-	buf[18] = encoding[(l>>35)&0x1f]
-	buf[19] = encoding[(l>>30)&0x1f]
-	buf[20] = encoding[(l>>25)&0x1f]
-	buf[21] = encoding[(l>>20)&0x1f]
-	buf[22] = encoding[(l>>15)&0x1f]
-	buf[23] = encoding[(l>>10)&0x1f]
-	buf[24] = encoding[(l>>5)&0x1f]
-	buf[25] = encoding[l&0x1f]
+	buf[10] = encoding[(c>>27)&0x1f]
+	buf[11] = encoding[(c>>22)&0x1f]
+	buf[12] = encoding[(c>>18)&0x1f]
+	buf[13] = encoding[(c>>13)&0x1f]
+	buf[14] = encoding[(c>>7)&0x1f]
+	buf[15] = encoding[(c>>2)&0x1f]
+	buf[16] = encoding[(d>>21)&0x1f]
+	buf[17] = encoding[(d>>16)&0x1f]
+	buf[18] = encoding[(d>>11)&0x1f]
+	buf[19] = encoding[(d>>6)&0x1f]
+	buf[20] = encoding[(d>>1)&0x1f]
+	buf[21] = encoding[(e>>20)&0x1f]
+	buf[22] = encoding[(e>>15)&0x1f]
+	buf[23] = encoding[(e>>10)&0x1f]
+	buf[24] = encoding[(e>>5)&0x1f]
+	buf[25] = encoding[e&0x1f]
 
 	return buf
 }
